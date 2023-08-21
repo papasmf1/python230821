c72e21f1985e1c8cf362c66d11c0f03652eaff4d
papasmf1 <papasmf1@gmail.com>
2023-08-21 16:39:27 +0900

    월요일 수업 완료

---
+++ function2.py
@@ -0,0 +1,19 @@
+# function2.py 
+#스코핑룰(이름 해석 규칙):LGB규칙 
+#전역변수
+x = 1 
+def func(a):
+    return a+x 
+
+#호출
+print(func(1))
+
+def func2(a):
+    #지역변수
+    x = 5 
+    return a+x 
+
+#호출
+print(func2(1))
+
+
